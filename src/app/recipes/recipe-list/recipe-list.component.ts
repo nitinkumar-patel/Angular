91b22a2e617f8d7e9b782155c00a7b9daf3ca210
Nitin <er.nitinpatel914@gmail.com>
2018-09-04 22:56:48 -0500

    deleting ingredient functionality

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[] = [];
+  subscription: Subscription;
 
   constructor(private recipeService: RecipeService,
               private router: Router,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.recipeService.recipeChanged.subscribe(
+    this.subscription = this.recipeService.recipeChanged.subscribe(
       (recipes: Recipe[]) => {this.recipes = recipes;}
     );
     this.recipes = this.recipeService.getRecipes();
@@ -25,5 +27,9 @@ export class RecipeListComponent implements OnInit {
 
   onNewRecipe() {
     this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 }
