13695ea2496553def9c3643276c0c282c497ef12
Nitin <er.nitinpatel914@gmail.com>
2018-09-06 00:08:22 -0500

    setting up signin and signup functionality with firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,11 @@ export class AppComponent implements OnInit {
   ngOnInit(){
     firebase.initializeApp({
       apiKey: "AIzaSyDx5rN1-m-W9dUD0ZxAom7vorhSdktCPgI",
-      authDomain: "ng-recipe-book-c3da3.firebaseapp.com"
+      authDomain: "ng-recipe-book-c3da3.firebaseapp.com",
+      databaseURL: "https://ng-recipe-book-c3da3.firebaseio.com",
+      projectId: "ng-recipe-book-c3da3",
+      storageBucket: "ng-recipe-book-c3da3.appspot.com",
+      messagingSenderId: "383575661438"
     });
 
   }
