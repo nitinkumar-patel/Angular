689316b94f7391cb3a168037086d3bef37c46902
Nitin <er.nitinpatel914@gmail.com>
2018-09-05 22:16:13 -0500

    sending put request to soredata into firebasedb

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { RecipeService } from "../recipes/recipe.service";
+
+@Injectable()
+export class DataStorageService {
+    constructor(private http: Http, private recipeService: RecipeService) {}
+
+    storeRecipes() {
+        return this.http.put('https://ng-recipe-book-c3da3.firebaseio.com/recipe.json', this.recipeService.getRecipes());
+    }
+}
