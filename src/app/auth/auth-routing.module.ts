a54d9815469901bbf0cc4a18389f6a0105f500c2
Nitin <er.nitinpatel914@gmail.com>
2018-09-06 22:39:45 -0500

    Creating Auth feature Module

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,16 @@
+import { RouterModule, Routes } from "@angular/router";
+import { SignupComponent } from "./signup/signup.component";
+import { SigninComponent } from "./signin/signin.component";
+import { NgModule } from "@angular/core";
+
+const authRoutes: Routes = [
+    { path: 'signup', component: SignupComponent},
+    { path: 'signin', component: SigninComponent} ]
+
+    @NgModule({
+    imports: [RouterModule.forChild(authRoutes)],
+    exports: [RouterModule]
+})
+export class AuthRoutingModule {
+
+}
